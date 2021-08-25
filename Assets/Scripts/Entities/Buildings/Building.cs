7c8466d7ee686cdb867b793ed1369a5c76f05b16
godzzz <g0dzZz1lla@yandex.ru>
2021-08-26 01:03:07 +0300

    + Level system for building

--- Assets/Scripts/Entities/Buildings/Building.cs
+++ Assets/Scripts/Entities/Buildings/Building.cs
@@ -1,17 +1,26 @@
 using UnityEngine;
+using System;
 
 namespace IdleGame.Entities.Buildings
 {
     using People;
     using Core;
+    using Player;
 
     public class Building : EntityBase<BuildingData>
     {
+        [SerializeField] private string _name = "Building";
         [SerializeField] private BuildingView _view = default;
         [SerializeField] private InteractionTrigger _trigger = default;
 
+        public string Name => _name;
+        public int Level { get; private set; }
+        public int Reward { get; private set; }
+        public int UpgradeCost { get; private set; }
+
         private void Start()
         {
+            SetLevel(Data.levels.initialLevel);
             _view.Init(this);
 
             _trigger.Interaction += OnInteraction;
@@ -22,10 +31,26 @@ namespace IdleGame.Entities.Buildings
             _trigger.Interaction -= OnInteraction;
         }
 
+        public void Upgrade()
+        {
+            Player.DecreaseScore(UpgradeCost);
+            SetLevel(Level + 1);
+        }
+
         private void OnInteraction(Human human)
         {
-            Player.Player.IncreaseScore(1);
+            Player.IncreaseScore(Reward);
             GameLogic.Instance.OnHumanDestinationReached(human);
+        }
+
+        private void SetLevel(int index)
+        {
+            if (index < 1)
+                throw new ArgumentOutOfRangeException();
+
+            Level = index;
+            Reward = Data.levels.initialReward + (Level - 1) * Data.levels.gainPerLevel;
+            UpgradeCost = Level * Data.levels.upgradeCost;
         }
     }
 }
--- Assets/Scripts/Entities/Buildings/BuildingData.cs
+++ Assets/Scripts/Entities/Buildings/BuildingData.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 namespace IdleGame.Entities.Buildings
@@ -5,6 +6,19 @@ namespace IdleGame.Entities.Buildings
     [CreateAssetMenu(fileName = "Building", menuName = "Entities/Building", order = 51)]
     public class BuildingData : EntityData
     {
+        public LevelSettings levels = new LevelSettings();
 
+        [Serializable]
+        public class LevelSettings
+        {
+            [Min(1)]
+            public int initialLevel = 0;
+            [Min(1)]
+            public int initialReward = 1;
+            [Min(0)]
+            public int upgradeCost = 5;
+            [Min(0)]
+            public int gainPerLevel = 2;
+        }
     }
 }
